bac4dcae7a0c9edd9c0b6afb32058f9781719bd0
mairisaar <mairi.saar@khk.ee>
2018-10-11 13:56:39 +0300

    Lisatud numbrifaktide kuvamise funktsionaalsus.

---
+++ src/components/Faktid.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Faktid numbrite kohta</h1>
+    <p>Huvitavad faktid numbrite kohta</p>
+    <input type="number" min="0" v-model="number">
+    <p v-cloak>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+
+export default {
+  name: 'Faktid',
+  data () {
+    return {
+      data: '',
+      number: 0
+    }
+  },
+  watch: {
+    number() {
+      if (this.number !== '') {
+        let url = `http://numbersapi.com/${this.number}`
+        
+        axios.get(url)
+          .then(response => {
+            this.data = response.data
+          })
+          .catch(error => {
+            console.log(error)
+        })
+      }
+    }
+  },
+  methods: { 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+</style>
